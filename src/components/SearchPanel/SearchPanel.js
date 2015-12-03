c9fdc537c20e03f71112b2b3852cacd857be90bd
Nix - Eugene Timokhin <evgeniy.timokhin@nixsolutions.com>
2015-12-03 16:02:40 +0200

    - added serch button handler;

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -7,15 +7,25 @@ class SearchPanel extends Component {
     super(props);
   }
 
+  _onSearchHandler() {
+    var searchValue = this.searchInput.value;
+    console.log(searchValue)
+    if (!searchValue) {
+      return;
+    }
+  }
+
   render() {
+    var self = this;
+
     return (
       <div styleName="header" className="container-fluid">
         <h2 styleName="title">Stuffinder</h2>
         <div className="row">
           <div styleName="search-panel" className="input-group col-md-offset-4 col-md-4">
-            <input type="text" className="form-control" placeholder="Search..."/>
+            <input type="text" ref={ (ref) => self.searchInput = ref } className="form-control" placeholder="Search..."/>
             <div className="input-group-btn">
-              <button type="button" className="btn btn-default">Go!</button>
+              <button type="button" onClick={self._onSearchHandler.bind(this)} className="btn btn-default">Go!</button>
             </div>
           </div>
         </div>
